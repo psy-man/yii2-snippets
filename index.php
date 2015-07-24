d995cd315ba2cbe753ac9c14b29618d4bdc64030
Evgeniy Filipyev <w.0.w@mail.ru>
2015-07-24 17:29:41 +0300

    v 1.1

--- index.php
+++ index.php
@@ -18,11 +18,14 @@ class SnippetsGenerator
 XML;
 
 	public static $snippets = [
+
+		// HTML
 		[
 			'class' 		=> 'Html',
 			'prefix'		=> 'H',
 			'scope' 		=> 'text.html',
 			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
 			'template' 		=> '<?= {class}::{name}({args}); ?>',
 			'items' => [
 				'e'        => 'encode($content)',
@@ -67,9 +70,10 @@ XML;
 		],
 		[
 			'class' 		=> 'ActiveField',
-			'prefix'		=> 'F',
+			'prefix'		=> 'AF',
 			'scope' 		=> 'text.html',
 			'description' 	=> '$form->field()->{name}',
+			'startKey'		=> 2,
 			'template' 		=> '<?= $form->field(${1:\$model}, \'${2:attribute}\')->{name}({args}); ?>',
 			'items' => [
 				'ti'       => 'textInput($options = [])',
@@ -86,20 +90,132 @@ XML;
 	            'w'        => 'widget($class, $config = [])',
 			],
 		],
+		[
+			'class' 		=> 'HtmlPurifier',
+			'prefix'		=> 'HP',
+			'scope' 		=> 'text.html',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> '<?= {class}::{name}({args}); ?>',
+			'items' => [
+				'np' 		=> 'process($path, $ds = DIRECTORY_SEPARATOR)',
+			],
+		],
+
+		// PHP
+		[
+			'class' 		=> 'ArrayHelper',
+			'prefix'		=> 'AH',
+			'scope' 		=> 'text.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> '{class}::{name}({args});',
+			'items' => [
+				'ta' 		=> 'toArray($object, $properties = [], $recursive = true)',
+				'm' 		=> 'merge($a, $b)',
+				'gv' 		=> 'getValue($array, $key, $default = null)',
+				'r' 		=> 'remove(&$array, $key, $default = null)',
+				'i' 		=> 'index($array, $key)',
+				'gc'		=> 'getColumn($array, $name, $keepKeys = true)',
+				'map' 		=> 'map($array, $from, $to, $group = null)',
+				'ke' 		=> 'keyExists($key, $array, $caseSensitive = true)',
+				'sort' 		=> 'multisort(&$array, $key)',
+				'e' 		=> 'htmlEncode($data, $valuesOnly = true, $charset = null)'
+			],
+		],
+		[
+			'class' 		=> 'FileHelper',
+			'prefix'		=> 'FH',
+			'scope' 		=> 'text.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> '{class}::{name}({args});',
+			'items' => [
+				'np' 		=> 'normalizePath($path, $ds = DIRECTORY_SEPARATOR)',
+				'l' 		=> 'localize($file, $language = null, $sourceLanguage = null)',
+				'gmt' 		=> 'getMimeType($file, $magicFile = null, $checkExtension = true)',
+				'gmtbe' 	=> 'getMimeTypeByExtension($file, $magicFile = null)',
+				'gebmt' 	=> 'getExtensionsByMimeType($mimeType, $magicFile = null)',
+				'cpd' 		=> 'copyDirectory($src, $dst, $options = [])',
+				'rd' 		=> 'removeDirectory($dir, $options = [])',
+				'ff' 		=> 'findFiles($dir, $options = [])',
+				'fp' 		=> 'filterPath($path, $options)',
+				'cd'	 	=> 'createDirectory($path, $mode = 0775, $recursive = true)',
+			],
+		],
+		[
+			'class' 		=> 'Url',
+			'prefix'		=> 'U',
+			'scope' 		=> 'text.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> '{class}::{name}({args})',
+			'items' => [
+				'tr' 		=> 'toRoute($route, $scheme = false)',
+				't' 		=> 'to($url, $scheme = false)',
+				'b' 		=> 'base($scheme = false)',
+				'r' 		=> 'remember($url, $name = null)',
+				'p' 		=> 'previous($name = null)',
+				'c' 		=> 'canonical()',
+				'h' 		=> 'home($scheme = false)',
+				'ir' 		=> 'isRelative($url)',
+				'c' 		=> 'current(array $params = [], $scheme = false)',
+			],
+		],
+		[
+			'class' 		=> 'Json',
+			'prefix'		=> 'J',
+			'scope' 		=> 'text.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> '{class}::{name}({args})',
+			'items' => [
+				'e' 		=> 'encode($value)',
+				'he' 		=> 'htmlEncode($value)',
+				'd' 		=> 'decode($json, $asArray = true)',
+			],
+		],
+		[
+			'class' 		=> 'StringHelper',
+			'prefix'		=> 'SH',
+			'scope' 		=> 'text.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> '{class}::{name}({args})',
+			'items' => [
+				'bl' 		=> 'byteLength($string)',
+				'bs' 		=> 'byteSubstr($string, $start, $length = null)',
+				'bn' 		=> 'basename($path, $suffix)',
+				'dn' 		=> 'dirname($path)',
+				'tr' 		=> 'truncate($string, $length, $suffix, $encode, $asHtml)',
+				'trw' 		=> 'truncateWords($string, $count, $suffix, $asHtml)',
+				'sw' 		=> 'startsWith($string, $with, $caseSensitive = true)',
+				'ew' 		=> 'endsWith($string, $with, $caseSensitive = true)',
+				'exp' 		=> 'explode($string, $delimiter, $trim = true, $skipEmpty = false)',
+			],
+		],
+
+		[
+			'class' 		=> 'Yii',
+			'prefix'		=> 'Y',
+			'scope' 		=> 'text.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> '{class}::{name}({args})',
+			'items' => [
+				't' 		=> 't($string)',
+			],
+		],
+
 
 	];
 
     public function build()
     {
-
-    	self::clean();
-
     	foreach (self::$snippets as $data) {
 
+    		self::rrmdir(getcwd() . '/' . $data['class']);
     		self::processSnippet($data);
-    		echo "<pre>";
-    			print_r("-----------------------------------------------------------------------------\n");
-    		echo "</pre>";
     	}
     }
 
@@ -110,7 +226,7 @@ XML;
     		preg_match('/\\(([^()]*)\\)/', $func, $result);
 
     		$name = str_replace($result[0], '', $func);
-        	$args = implode(',', self::processArgs($result[1]));
+        	$args = implode(',', self::processArgs($result[1], $data['startKey']));
 
         	$content = str_replace('{class}', $data['class'], $data['template']);
         	$content = str_replace('{name}', $name, $content);
@@ -120,7 +236,7 @@ XML;
         	$description = str_replace('{name}', $name, $description);
 
         	$snippet = str_replace('{content}', $content, self::$snippetTemplate);
-        	$snippet = str_replace('{tabTrigger}', $data['class'] . $trigger, $snippet);
+        	$snippet = str_replace('{tabTrigger}', $data['prefix'] . $trigger, $snippet);
         	$snippet = str_replace('{scope}', $data['scope'], $snippet);
         	$snippet = str_replace('{description}', $description, $snippet);
 
@@ -132,18 +248,18 @@ XML;
         		mkdir($data['class'], 0777, true);
         	}
 
-        	file_put_contents($data['class'] . '/' . 'text.sublime-snippet', $snippet);
+        	file_put_contents($data['class'] . '/' . $name . '.sublime-snippet', $snippet);
 
     	}
     }
 
 
-   	private static function processArgs($args)
+   	private static function processArgs($args, $plus = 0)
     {
     	$args = explode(',', $args);
 
     	foreach ($args as $key => &$arg) {
-            $arg = self::processArg($key, $arg);
+            $arg = self::processArg($key + $plus, $arg);
         }
 
         return $args;
@@ -164,15 +280,6 @@ XML;
 
 
 
-
-    private static function clean() {
-
-    	echo "<pre>";
-    		print_r(scandir('.'));
-    	echo "</pre>";
-    }
-
-
     private static function rrmdir($dir) {
       	if (is_dir($dir)) {
         	$objects = scandir($dir);
@@ -180,7 +287,7 @@ XML;
         	foreach ($objects as $object) {
          		if ($object != "." && $object != "..") {
             		if (filetype($dir."/".$object) == "dir")
-               			rrmdir($dir."/".$object);
+               			self::rrmdir($dir."/".$object);
             		else unlink   ($dir."/".$object);
           		}
         	}
