492e36b0eee1a08b41429a1f92c52f3bae435df0
Evgeniy Filipyev <w.0.w@mail.ru>
2015-07-29 12:15:52 +0300

    v 1.1

--- index.php
+++ index.php
@@ -19,6 +19,20 @@ XML;
 
 	public static $snippets = [
 
+		// Yii global snippets
+		[
+			'class' 		=> 'Yii',
+			'prefix'		=> 'Y',
+			'scope' 		=> 'source.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> '{class}::{name}({args})',
+			'items' => [
+				't' 		=> 't($category,$string)',
+			],
+		],
+
+
 		// HTML
 		[
 			'class' 		=> 'Html',
@@ -74,7 +88,7 @@ XML;
 			'scope' 		=> 'text.html',
 			'description' 	=> '$form->field()->{name}',
 			'startKey'		=> 2,
-			'template' 		=> '<?= $form->field(${1:\$model}, \'${2:attribute}\')->{name}({args}); ?>',
+			'template' 		=> '<?= \$form->field(${1:\$model}, \'${2:attribute}\')->{name}({args}); ?>',
 			'items' => [
 				'ti'       => 'textInput($options = [])',
 	            'hi'       => 'hiddenInput($options = [])',
@@ -98,7 +112,7 @@ XML;
 			'startKey'		=> 0,
 			'template' 		=> '<?= {class}::{name}({args}); ?>',
 			'items' => [
-				'np' 		=> 'process($path, $ds = DIRECTORY_SEPARATOR)',
+				'np' 		=> 'process($html)',
 			],
 		],
 
@@ -106,7 +120,7 @@ XML;
 		[
 			'class' 		=> 'ArrayHelper',
 			'prefix'		=> 'AH',
-			'scope' 		=> 'text.php',
+			'scope' 		=> 'source.php',
 			'description' 	=> "{class}::{name}",
 			'startKey'		=> 0,
 			'template' 		=> '{class}::{name}({args});',
@@ -126,7 +140,7 @@ XML;
 		[
 			'class' 		=> 'FileHelper',
 			'prefix'		=> 'FH',
-			'scope' 		=> 'text.php',
+			'scope' 		=> 'source.php',
 			'description' 	=> "{class}::{name}",
 			'startKey'		=> 0,
 			'template' 		=> '{class}::{name}({args});',
@@ -146,7 +160,7 @@ XML;
 		[
 			'class' 		=> 'Url',
 			'prefix'		=> 'U',
-			'scope' 		=> 'text.php',
+			'scope' 		=> 'source.php',
 			'description' 	=> "{class}::{name}",
 			'startKey'		=> 0,
 			'template' 		=> '{class}::{name}({args})',
@@ -165,7 +179,7 @@ XML;
 		[
 			'class' 		=> 'Json',
 			'prefix'		=> 'J',
-			'scope' 		=> 'text.php',
+			'scope' 		=> 'source.php',
 			'description' 	=> "{class}::{name}",
 			'startKey'		=> 0,
 			'template' 		=> '{class}::{name}({args})',
@@ -178,7 +192,7 @@ XML;
 		[
 			'class' 		=> 'StringHelper',
 			'prefix'		=> 'SH',
-			'scope' 		=> 'text.php',
+			'scope' 		=> 'source.php',
 			'description' 	=> "{class}::{name}",
 			'startKey'		=> 0,
 			'template' 		=> '{class}::{name}({args})',
@@ -194,20 +208,141 @@ XML;
 				'exp' 		=> 'explode($string, $delimiter, $trim = true, $skipEmpty = false)',
 			],
 		],
-
 		[
-			'class' 		=> 'Yii',
-			'prefix'		=> 'Y',
-			'scope' 		=> 'text.php',
+			'class' 		=> 'Request',
+			'prefix'		=> 'R',
+			'scope' 		=> 'source.php',
 			'description' 	=> "{class}::{name}",
 			'startKey'		=> 0,
-			'template' 		=> '{class}::{name}({args})',
+			'template' 		=> 'Yii::\$app->request->{name}({args})',
 			'items' => [
-				't' 		=> 't($string)',
+				'res' 		=> 'resolve()',
+				'h' 		=> 'getHeaders()',
+				'm' 		=> 'getMethod()',
+				'ig' 		=> 'getIsGet()',
+				'io' 		=> 'getIsOptions()',
+				'ih' 		=> 'getIsHead()',
+				'ip' 		=> 'getIsPost()',
+				'id' 		=> 'getIsDelete()',
+				'iput' 		=> 'getIsPut()',
+				'ipatch' 	=> 'getIsPatch()',
+				'ia' 		=> 'getIsAjax()',
+				'ipj' 		=> 'getIsPjax()',
+				'if' 		=> 'getIsFlash()',
+				'rb' 		=> 'getRawBody()',
+				'srb' 		=> 'setRawBody($rawBody)',
+				'bps' 		=> 'getBodyParams()',
+				'sbps' 		=> 'setBodyParams($values)',
+				'bp' 		=> 'getBodyParam($name, $defaultValue = null)',
+				'p' 		=> 'post($name = null, $defaultValue = null)',
+				'qps' 		=> 'getQueryParams()',
+				'sqps' 		=> 'setQueryParams($values)',
+				'g' 		=> 'get($name = null, $defaultValue = null)',
+				'qp' 		=> 'getQueryParam($name, $defaultValue = null)',
+				'hi' 		=> 'getHostInfo()',
+				'shi' 		=> 'setHostInfo($value)',
+				'bu' 		=> 'getBaseUrl()',
+				'sbu' 		=> 'setBaseUrl($value)',
+				'au' 		=> 'getAbsoluteUrl()',
+				'u' 		=> 'getUrl()',
+				'su' 		=> 'setUrl($value)',
+				'qs' 		=> 'getQueryString()',
+				'isc' 		=> 'getIsSecureConnection()',
+				'sm' 		=> 'getServerName()',
+				'sp' 		=> 'getServerPort()',
+				'ref' 		=> 'getReferrer()',
+				'ua' 		=> 'getUserAgent()',
+				'uip' 		=> 'getUserIP()',
+				'uh' 		=> 'getUserHost()',
+				'cook' 		=> 'getCookies()',
+				'csrf' 		=> 'getCsrfToken($regenerate = false)',
+				'csrfh' 	=> 'getCsrfTokenFromHeader()',
+				'csrfv' 	=> 'validateCsrfToken($token = null)',
 			],
 		],
+		[
+			'class' 		=> 'User',
+			'prefix'		=> 'U',
+			'scope' 		=> 'source.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> 'Yii::\$app->user->{name}({args})',
+			'items' => [
+				'i' 		=> 'getIdentity()',
+				'si' 		=> 'setIdentity($identity)',
+				'l' 		=> 'login($identity, $duration = 0)',
+				'lbat' 		=> 'loginByAccessToken($token, $type = null)',
+				'logout' 	=> 'logout($destroySession = true)',
+				'ig' 		=> 'getIsGuest()',
+				'id' 		=> 'getId()',
+				'gru' 		=> 'getReturnUrl($defaultUrl = null)',
+				'sru' 		=> 'setReturnUrl($url)',
+				'lr' 		=> 'loginRequired($checkAjax = true)',
+				'si'		=> 'switchIdentity($identity, $duration = 0)',
+				'can' 		=> 'can(\'$permissionName\', $params = [], $allowCaching = true)',
+			],
+		],
+		[
+			'class' 		=> 'Session',
+			'prefix'		=> 'S',
+			'scope' 		=> 'source.php',
+			'description' 	=> "{class}::{name}",
+			'startKey'		=> 0,
+			'template' 		=> 'Yii::\$app->session->{name}({args})',
+			'items' => [
+				'op' 		=> 'open()',
+				'cl' 		=> 'close()',
+				'desr' 		=> 'destroy()',
+				'ia' 		=> 'getIsActive()',
+				'hsi' 		=> 'getHasSessionId()',
+				'shsi' 		=> 'setHasSessionId($value)',
+				'id' 		=> 'getId()',
+				'sid' 		=> 'setId($value)',
+				'rg' 		=> 'regenerateID($deleteOldSession = false)',
+				'name' 		=> 'getName()',
+				'sname' 	=> 'setName($value)',
+				'sp' 		=> 'getSavePath()',
+				'ssp' 		=> 'setSavePath($value)',
+				'cp' 		=> 'getCookieParams()',
+				'scp' 		=> 'setCookieParams(array $value)',
+				'uc'		=> 'getUseCookies()',
+				'suc' 		=> 'setUseCookies($value)',
+				'GCP' 		=> 'getGCProbability()',
+				'sGCP' 		=> 'setGCProbability($value)',
+				'utsi' 		=> 'getUseTransparentSessionID()',
+				'sutsi' 	=> 'setUseTransparentSessionID($value)',
+				'to' 		=> 'getTimeout()',
+				'sto' 		=> 'setTimeout($value)',
+				'os' 		=> 'openSession($savePath, $sessionName)',
+				'cs' 		=> 'closeSession()',
+				'read' 		=> 'readSession($id)',
+				'write' 	=> 'writeSession($id, $data)',
+				'destroy' 	=> 'destroySession($id)',
+				'gcs' 		=> 'gcSession($maxLifetime)',
+				'iter' 		=> 'getIterator()',
+				'count' 	=> 'getCount()',
+				'get' 		=> 'get($key, $defaultValue = null)',
+				'set' 		=> 'set($key, $value)',
+				'remove' 	=> 'remove($key)',
+				'removeAll' => 'removeAll()',
+				'has' 		=> 'has($key)',
+				'fl' 		=> 'getFlash($key, $defaultValue = null, $delete = false)',
+				'afl' 		=> 'getAllFlashes($delete = false)',
+				'sfl' 		=> 'setFlash($key, $value = true, $removeAfterAccess = true)',
+				'afl' 		=> 'addFlash($key, $value = true, $removeAfterAccess = true)',
+				'rfl' 		=> 'removeFlash($key)',
+				'rafl' 		=> 'removeAllFlashes()',
+				'hfl' 		=> 'hasFlash($key)',
+				'oe' 		=> 'offsetExists($offset)',
+				'og' 		=> 'offsetGet($offset)',
+				'os' 		=> 'offsetSet($offset, $item)',
+				'ou' 		=> 'offsetUnset($offset)',
+			],
+		],
+	];
 
-
+	public static $customSnippets = [
+		'U'			=> 'Yii::\$app->user',
 	];
 
     public function build()
@@ -226,9 +361,16 @@ XML;
     		preg_match('/\\(([^()]*)\\)/', $func, $result);
 
     		$name = str_replace($result[0], '', $func);
-        	$args = implode(',', self::processArgs($result[1], $data['startKey']));
+        	$args = implode(', ', self::processArgs($result[1], $data['startKey']));
+
 
         	$content = str_replace('{class}', $data['class'], $data['template']);
+
+        	if (empty($args) && strpos($name, 'get') === 0) {
+        		$name = lcfirst(str_replace('get', '', $name));
+        		$content = str_replace('({args})', '', $content);
+        	}
+
         	$content = str_replace('{name}', $name, $content);
         	$content = str_replace('{args}', $args, $content);
 
@@ -240,9 +382,14 @@ XML;
         	$snippet = str_replace('{scope}', $data['scope'], $snippet);
         	$snippet = str_replace('{description}', $description, $snippet);
 
-        	// echo "<pre>";
-        	// 	print_r(htmlspecialchars($content));
-        	// echo "</pre>";
+        	if ($data['class'] == 'Session') {
+        		echo "<pre>";
+        			print_r(htmlspecialchars($content));
+        		echo "</pre>";
+        	}
+
+
+
 
         	if (!file_exists($data['class'])) {
         		mkdir($data['class'], 0777, true);
@@ -257,6 +404,9 @@ XML;
    	private static function processArgs($args, $plus = 0)
     {
     	$args = explode(',', $args);
+
+    	if (count($args) == 1 && empty($args[0]))
+    		return [];
 
     	foreach ($args as $key => &$arg) {
             $arg = self::processArg($key + $plus, $arg);
@@ -273,6 +423,10 @@ XML;
         if($arg == 'options') {
 
             return '${' . ($key + 1) . ':[\'${' . ($key + 2) . ':option}\' => ${' . ($key + 3) . ':value}]}';
+        }
+
+        if($arg[0] == "'") {
+        	return '\'${' . ($key + 1) . ':'. trim($arg, '\'') . '}\'';
         }
 
         return '${' . ($key + 1) . ':'. $arg . '}';
